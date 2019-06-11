3c3e8ca0d62d563c5183ebbb1f83ae758933ff57
EliasCF <edimbo4@gmail.com>
2019-06-11 08:47:44 +0200

    First commit

---
+++ Udlånsregistrering/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Udlånsregistrering.Data;
+
+[assembly: HostingStartup(typeof(Udlånsregistrering.Areas.Identity.IdentityHostingStartup))]
+namespace Udlånsregistrering.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
